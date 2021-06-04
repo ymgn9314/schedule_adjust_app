608cad8c1afa302f5d254b88fd5639d5af2f8a6e
ymgn9314 <yamagen.private@gmail.com>
2021-06-04 19:01:34 +0900

    Userドメイン同士の依存解決

--- lib/domain/user/user_factory_base.dart
+++ lib/domain/user/user_factory_base.dart
@@ -1,7 +1,9 @@
+import 'package:high_hat/domain/schedule/value/schedule_date.dart';
+import 'package:high_hat/domain/schedule/value/schedule_id.dart';
 import 'package:high_hat/domain/user/user.dart';
-import 'package:high_hat/domain/user/value/answers.dart';
+import 'package:high_hat/domain/user/value/answer.dart';
 import 'package:high_hat/domain/user/value/avatar_url.dart';
-import 'package:high_hat/domain/user/value/user_friend.dart';
+import 'package:high_hat/domain/user/value/user_id.dart';
 import 'package:high_hat/domain/user/value/user_name.dart';
 import 'package:high_hat/domain/user/value/user_profile_id.dart';
 
@@ -12,7 +14,7 @@ abstract class UserFactoryBase {
     required UserName userName,
     required UserProfileId userProfileId,
     required AvatarUrl avatarUrl,
-    required UserFriend userFriend,
-    required AnswersToSchedule answersToSchedule,
+    required List<UserId> userFriend,
+    required Map<ScheduleId, Map<ScheduleDate, Answer>> answersToSchedule,
   });
 }
